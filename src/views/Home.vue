84690ea7ae2e58a020b1bd0b7c3affdc1c557720
colin <colin.legoedec@laposte.net>
2021-07-08 01:28:08 +0200

    Initial Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <p class="rellax"  data-rellax-speed="7">Para 1</p>
+
+    <div class="rellax" data-rellax-speed="-6">
+       I parallax at all different speeds depending on your screen width.
+    </div>
+
+    <p class="rellax"  data-rellax-speed="15">Para </p> -->
+    <div class="img1 rellax" data-rellax-speed="-6">sv</div>
+    <div class="img2 rellax" data-rellax-speed="7">">vsdv</div>
+    <div class="img3 rellax">sdvsd</div>
+    <div class="img4 rellax">sdvsd</div>
   </div>
+
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import Rellax from 'rellax'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  // data(){
+  //   return(){
+  //     rellax: null
+  //   }
+  // },
+  mounted(){
+    this.rellax = new Rellax('.rellax')
   }
 }
 </script>
+
+<style lang="css">
+  p{
+    font-size: 6rem;
+  }
+
+  .img1{
+    height: 6rem; width:20rem;
+    background: url('../assets/img1.svg') no-repeat center center/contain;
+    margin-bottom: 5rem;
+  }
+  .img2{
+    height: 6rem; width:20rem; margin-left : 20rem;
+    background: url('../assets/img2.png') no-repeat center center/contain;
+    margin-bottom: 5rem;
+  }
+  .img3{
+    height: 6rem; width:20rem; margin-left: 40rem;
+    background: url('../assets/img3.jpg') no-repeat center center/contain;
+    margin-bottom: 20rem;
+  }
+  /* .img4{
+    height: 6rem; width:20rem;
+    background: url('../assets/img4.jpg  ') no-repeat center center contain;
+  } */
+</style>
